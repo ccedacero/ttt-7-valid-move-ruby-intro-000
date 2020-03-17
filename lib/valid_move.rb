03f34aedd78a221746176e86ed23585766caae6b
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-03-17 03:30:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board,indx)
   index = indx.to_i;
+if board[index] != '' || board[index] !=' '
+  return false
+end
+
 if index >= 0 && index <= 8
   return true;
 elsif board[index] == '' || board[index] == ' '
